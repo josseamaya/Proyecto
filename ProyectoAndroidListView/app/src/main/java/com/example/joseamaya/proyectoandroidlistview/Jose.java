dad5f04e7de81ebe55103e003ae6ece15f4eae26
josseamaya <josseamaya@gmail.com>
2016-03-11 11:10:11 -0600

    jaj

--- ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Jose.java
+++ ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Jose.java
@@ -5,6 +5,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Jose extends AppCompatActivity {
@@ -18,6 +30,7 @@ public class Jose extends AppCompatActivity {
         mContext = this;
 
         String url = "http://api.kivaws.org/v1/loans/newest.json";
+
 
     }
 
