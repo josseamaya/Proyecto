9cf9406373fd6ffa3e970428e3f4f0e3abb67b93
josseamaya <josseamaya@gmail.com>
2016-03-11 11:09:37 -0600

    sigo arreglando algunos problemas

--- ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Jose.java
+++ ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Jose.java
@@ -4,23 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.example.joseamaya.proyectoandroidlistview.R.id.textViewJoseNombre;
 
 
 public class Jose extends AppCompatActivity {
@@ -34,6 +18,7 @@ public class Jose extends AppCompatActivity {
         mContext = this;
 
         String url = "http://api.kivaws.org/v1/loans/newest.json";
+
     }
 
 
