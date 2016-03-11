f38cbc0d62f8cd8556e3157f252ac28d8d594ed7
josseamaya <josseamaya@gmail.com>
2016-03-11 09:48:06 -0600

    ok

--- ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Jose.java
+++ ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Jose.java
@@ -1,21 +1,73 @@
 package com.example.joseamaya.proyectoandroidlistview;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.joseamaya.proyectoandroidlistview.R.id.textViewJoseNombre;
+
 
 public class Jose extends AppCompatActivity {
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jose);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mContext=this;
 
     }
+    public void kivaLlenarInformacion(){
+        String url = "http://api.kivaws.org/v1/loans/newest.json";
+    public void kivaLlenarList(){
+        String url = "http://api.kivaws.org/v1/loans/newest.json";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        String loans = null;
+                        try {
+                            loans = response.getString("loans");
+                            JSONArray arregloPersonas = new JSONArray(loans);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+        MySingleton.getInstance(MainActivity.mContext).addToRequestQueue(jsObjRequest);
+    }
+
+    }
+
 
 }
