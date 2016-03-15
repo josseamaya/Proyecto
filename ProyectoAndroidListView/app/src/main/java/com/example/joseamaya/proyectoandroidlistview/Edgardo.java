b652f6a487ce40295b0c1d2dabdc9cc72f0c44d9
Edgardo España <edgardo.espana@ujcv.edu.hn>
2016-03-15 12:09:22 -0600

    Codigo de los Botones

--- ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Edgardo.java
+++ ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Edgardo.java
@@ -1,8 +1,14 @@
 package com.example.joseamaya.proyectoandroidlistview;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,6 +24,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Edgardo extends AppCompatActivity {
+    String LlamarEdgardoTelefono = "";
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +55,25 @@ public class Edgardo extends AppCompatActivity {
 
                             TextView nombres=(TextView)findViewById(R.id.textViewEdgardoNombres);
                             nombres.setText(edgardo.getString("nombre").toString());
+
                             TextView apellidos=(TextView)findViewById(R.id.textViewEdgardoApellidos);
                             apellidos.setText(edgardo.getString("apellido").toString());
+
                             TextView telefono=(TextView)findViewById(R.id.textViewEdgardoTelefono);
                             telefono.setText(edgardo.getString("telefono").toString());
+
                             TextView correo=(TextView)findViewById(R.id.textViewEdgardoCorreo);
                             correo.setText(edgardo.getString("email").toString());
+
                             TextView facebook=(TextView)findViewById(R.id.textViewEdgardoFacebook);
                             facebook.setText(edgardo.getString("facebook").toString());
+
                             TextView descripcion=(TextView)findViewById(R.id.textViewEdgardoDescripcion);
                             descripcion.setText(edgardo.getString("descripcion").toString());
-                            NetworkImageView niv=(NetworkImageView)findViewById(R.id.imageViewEdgardo);
+
+                            NetworkImageView FotoEdgardo=(NetworkImageView)findViewById(R.id.imageViewEdgardo);
                             String image=edgardo.getString("imagen");
-                            niv.setImageUrl("http://imgur.com/"+image+".jpg",MySingleton.getInstance(MainActivity.mContext).getImageLoader());
+                            FotoEdgardo.setImageUrl("http://imgur.com/"+image+".jpg",MySingleton.getInstance(MainActivity.mContext).getImageLoader());
 
                         }catch (JSONException e){
                             e.printStackTrace();
@@ -78,4 +92,34 @@ public class Edgardo extends AppCompatActivity {
         MySingleton.getInstance(this).addToRequestQueue(responseEdgardo);
     }
 
+   /* public void onClickEdgardoLlamar(View v){
+
+
+        Intent callIntent=new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(LlamarEdgardoTelefono));
+        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE))!= PackageManager.PERMISSION_GRANTED{
+            return;
+        }
+        startActivity(callIntent);
+
+    }*/
+
+    public void onClickSMSEdgardo (View v){
+        Intent IntentSMS= new Intent(Intent.ACTION_VIEW);
+        IntentSMS.setType("vnd.android-dir/mms-sms");
+        IntentSMS.putExtra("address", LlamarEdgardoTelefono);
+        IntentSMS.putExtra("sms_body", "");
+        startActivity(IntentSMS);
+
+    }
+
+    public void onClickWhatsappEdgardo(View v){
+        Uri uri=Uri.parse("SMSTo:"); //Falta agregar el telefono (duda)
+        Intent Whatsapp=new Intent(Intent.ACTION_SENDTO,uri);
+        Whatsapp.putExtra("sms_body","");
+        Whatsapp.setPackage("com.whatsapp");
+        startActivity(Whatsapp);
+
+    }
 }
+
