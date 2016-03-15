e4ef8355a8db9358292f8924f3bb749d73bc44a6
Edgardo España <edgardo.espana@ujcv.edu.hn>
2016-03-15 12:13:16 -0600

    nose

--- ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Edgardo.java
+++ ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Edgardo.java
@@ -1,14 +1,11 @@
 package com.example.joseamaya.proyectoandroidlistview;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
