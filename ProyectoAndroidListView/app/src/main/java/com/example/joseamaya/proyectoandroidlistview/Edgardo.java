36f7dbb7704dc709a8570ef7b5620f9359b65098
Edgardo España <edgardo.espana@ujcv.edu.hn>
2016-03-15 10:28:07 -0600

    Codigo Java de la Actividad Edgardo

--- ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Edgardo.java
+++ ProyectoAndroidListView/app/src/main/java/com/example/joseamaya/proyectoandroidlistview/Edgardo.java
@@ -6,6 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Edgardo extends AppCompatActivity {
 
@@ -24,6 +34,48 @@ public class Edgardo extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        String url="https://api.myjson.com/bins/4flfz";
+
+        JsonObjectRequest responseEdgardo=new JsonObjectRequest(url,
+                new Response.Listener< JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONArray usuarios=response.getJSONArray("usuarios");
+                            JSONObject edgardo=usuarios.getJSONObject(0);
+
+                            TextView nombres=(TextView)findViewById(R.id.textViewEdgardoNombres);
+                            nombres.setText(edgardo.getString("nombre").toString());
+                            TextView apellidos=(TextView)findViewById(R.id.textViewEdgardoApellidos);
+                            apellidos.setText(edgardo.getString("apellido").toString());
+                            TextView telefono=(TextView)findViewById(R.id.textViewEdgardoTelefono);
+                            telefono.setText(edgardo.getString("telefono").toString());
+                            TextView correo=(TextView)findViewById(R.id.textViewEdgardoCorreo);
+                            correo.setText(edgardo.getString("email").toString());
+                            TextView facebook=(TextView)findViewById(R.id.textViewEdgardoFacebook);
+                            facebook.setText(edgardo.getString("facebook").toString());
+                            TextView descripcion=(TextView)findViewById(R.id.textViewEdgardoDescripcion);
+                            descripcion.setText(edgardo.getString("descripcion").toString());
+                            NetworkImageView niv=(NetworkImageView)findViewById(R.id.imageViewEdgardo);
+                            String image=edgardo.getString("imagen");
+                            niv.setImageUrl("http://imgur.com/"+image+".jpg",MySingleton.getInstance(MainActivity.mContext).getImageLoader());
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+
+                    }
+                }
+        );
+
+        MySingleton.getInstance(this).addToRequestQueue(responseEdgardo);
     }
 
 }
